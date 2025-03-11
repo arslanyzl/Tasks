51d1511b8db81d47b935f933cd3fefaeedb4b31d
Arslan Yazlyyev <arslanyazlyyev@Mac.attlocal.net>
2025-03-10 22:01:33 -0500

    icon

---
+++ Tasks/AppTheme.swift
@@ -0,0 +1,27 @@
+//
+//  AppTheme.swift
+//  Tasks
+//
+//  Created by Arslan Yazlyyev on 3/9/25.
+//
+
+import SwiftUI
+
+struct AppTheme {
+    static let primary = Color(red: 0.2, green: 0.5, blue: 0.9)
+    static let secondary = Color(red: 0.9, green: 0.3, blue: 0.5)
+    static let background = Color(red: 0.95, green: 0.95, blue: 0.97)
+    static let cardBackground = Color.white
+    static let text = Color(red: 0.2, green: 0.2, blue: 0.3)
+    
+    static let taskColors: [Color] = [
+        .blue,
+        .green,
+        .orange,
+        .purple,
+        .pink,
+        .teal,
+        .indigo,
+        .red
+    ]
+}
